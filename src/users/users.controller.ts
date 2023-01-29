64015d7bdcf93b56bcc21ff6d90ae52f5ba2546d
Sebastián Pérez Etchandy <sebastian@peretch.com>
2023-01-28 21:21:33 -0300

    Added remove and update methods to users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,8 +6,10 @@ import {
   Patch,
   Param,
   Query,
+  Delete,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -29,5 +31,15 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
