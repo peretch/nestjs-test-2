3d8f57bc58bc960eee41ff9d946a804cfd216d03
Sebastián Pérez Etchandy <sebastian@peretch.com>
2023-01-28 21:23:08 -0300

    we

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
